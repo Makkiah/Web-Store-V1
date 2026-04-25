a0b091be4d42b9c567a291dca69cdb0429812365
Makkiah Muhammad <makkiahf@gmail.com>
2026-04-25 15:22:23 -0400

    april

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Setup
+const pool = require("./db")
 const express = require("express")
 
 const app = express()
@@ -7,16 +8,16 @@ app.use(express.urlencoded({ extended: true })) // This is middleware that parse
 
 // POST Methods
 
-app.post("/signup", (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password){
-    return res.send("All fields required!");
-  }
-  console.log("New Signup:")
-  console.log("Email: " + email)
-  console.log("Password: " + password)
-  res.redirect("/dashboard")
-})
+// app.post("/signup", (req, res) => {
+//   const { email, password } = req.body;
+//   if (!email || !password){
+//     return res.send("All fields required!");
+//   }
+//   console.log("New Signup:")
+//   console.log("Email: " + email)
+//   console.log("Password: " + password)
+//   res.redirect("/dashboard")
+// })
 
 app.post("/login", (req, res) => {
   const {email, password} = req.body;
@@ -26,6 +27,33 @@ app.post("/login", (req, res) => {
   console.log("Email: " + email)
   console.log("Password: " + password)
   res.redirect("/dashboard")
+})
+
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.send("All fields required")
+  }
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+      [email, password]
+    )
+
+    console.log(`New User Created => Email: ${email}, Password: ${password}`)
+
+    res.redirect("/dashboard")
+  } catch (err) {
+    console.error(err)
+
+    if (err.code === "23505") {
+      return res.send("User already exists")
+    }
+
+    res.send("Error creating user")
+  }
 })
 
 // GET Methods
@@ -43,7 +71,14 @@ app.get("/signup", (req, res) => {
 app.get("/dashboard", (req, res) => {
   res.sendFile(__dirname + "/views/dashboard.html")
 })
-
+app.get("/users", async (req, res) => {
+  const result = await pool.query("SELECT * FROM users")
+  res.json(result.rows)
+})
+aapp.get("/makkiah", async (req, res) => {
+  const result = await pool.query("SELECT * FROM users WHERE email = 'makkiahf@gmail.com")
+  res.json(result)
+})
 
 // Listen
 app.listen(3000, () => {
